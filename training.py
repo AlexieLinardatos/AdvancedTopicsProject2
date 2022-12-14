df9a2e237a3fc7bc6f5a7a203e9cf9eafca41da3
Alexie Linardatos <61524049+AlexieLinardatos@users.noreply.github.com>
2022-12-14 12:23:17 -0500

    Update training.py

--- training.py
+++ training.py
@@ -8,66 +8,6 @@ from keras.layers import Conv2D, Dropout, MaxPooling2D, Flatten, Dense, Activati
 import tensorflow as tf
 from datetime import datetime
 
-def get_result(result):
-
-    highest = result[0][0]
-    for x in range(len(result[0])):
-        if highest < result[0][x]:
-            highest = result[0][x]
-
-    if result[0][0] == highest:
-        return('a')
-    elif result[0][1] == highest:
-        return ('b')
-    elif result[0][2] == highest:
-        return ('c')
-    elif result[0][3] == highest:
-        return ('d')
-    elif result[0][4] == highest:
-        return ('e')
-    elif result[0][5] == highest:
-        return ('f')
-    elif result[0][6] == highest:
-        return ('g')
-    elif result[0][7] == highest:
-        return ('h')
-    elif result[0][8] == highest:
-        return ('i')
-    elif result[0][9] == highest:
-        return ('j')
-    elif result[0][10] == highest:
-        return ('k')
-    elif result[0][11] == highest:
-        return ('l')
-    elif result[0][12] == highest:
-        return ('m')
-    elif result[0][13] == highest:
-        return ('n')
-    elif result[0][14] == highest:
-        return ('o')
-    elif result[0][15] == highest:
-        return ('p')
-    elif result[0][16] == highest:
-        return ('q')
-    elif result[0][17] == highest:
-        return ('r')
-    elif result[0][18] == highest:
-        return ('s')
-    elif result[0][19] == highest:
-        return ('t')
-    elif result[0][20] == highest:
-        return ('u')
-    elif result[0][21] == highest:
-        return ('v')
-    elif result[0][22] == highest:
-        return ('w')
-    elif result[0][23] == highest:
-        return ('x')
-    elif result[0][24] == highest:
-        return ('y')
-    elif result[0][25] == highest:
-        return ('z')
-
         
 
 train_datagen = ImageDataGenerator(rescale = 1./255,
@@ -87,75 +27,75 @@ evalTrainAcc = []
 numUnits = []
 time = []
 
-for x in range(1,1000,100):
-    if(x>=2000):
-        break
-    start = datetime.now()
+# for x in range(1,1000,100):
+#     if(x>=2000):
+#         break
+start = datetime.now()
 
-    train_generator = train_datagen.flow_from_directory(
-    directory = 'Training',
-    target_size = (32,32),
-    batch_size = x,    #try changing this
-    class_mode = 'categorical'
+train_generator = train_datagen.flow_from_directory(
+directory = 'Training',
+target_size = (32,32),
+batch_size = 100,    #try changing this
+class_mode = 'categorical'
 
-    )
+)
 
-    test_generator = test_datagen.flow_from_directory(
-    directory = 'Testing',
-    target_size = (32,32),
-    batch_size = x,       # try changing this 
-    class_mode = 'categorical'
+test_generator = test_datagen.flow_from_directory(
+directory = 'Testing',
+target_size = (32,32),
+batch_size = 100,       # try changing this 
+class_mode = 'categorical'
 
-    )
-    model = Sequential()
-    model.add(Conv2D(32, (3, 3), input_shape = (32,32,3), activation = 'relu'))
-    model.add(MaxPooling2D(pool_size = (2, 2), strides =1))
+)
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape = (32,32,3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (2, 2), strides =1))
 
-    model.add(Conv2D(32, (3, 3), activation = 'relu'))
-    model.add(MaxPooling2D(pool_size = (2, 2), strides =1))
+model.add(Conv2D(32, (3, 3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (2, 2), strides =1))
 
-    model.add(Flatten())
-    #model.add(Dropout(0.25))
-    model.add(Dense(units = 126, activation = 'relu'))
-    model.add(Dense(units = 26, activation = 'softmax'))
+model.add(Flatten())
+#model.add(Dropout(0.25))
+model.add(Dense(units = 126, activation = 'relu'))
+model.add(Dense(units = 26, activation = 'softmax'))
 
 
-    model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
-    model.summary()
+model.summary()
 
-    history = model.fit(train_generator,
-                steps_per_epoch = 500,
-                epochs = 12,
-                validation_data = test_generator,
-                validation_steps = 16,
-                shuffle=True
-                )
-    evalTrainLoss = history.history['accuracy']
-    test_loss, test_acc = model.evaluate(test_generator, verbose=2) 
-    evalTestAcc.append(test_acc)
-    evalTestLoss.append(test_loss)
-    numUnits.append(x)
-    time.append(datetime.now() - start)
-    print(time,x)
-    model.save('trainedModel.h5')
+history = model.fit(train_generator,
+            steps_per_epoch = 500,
+            epochs = 12,
+            validation_data = test_generator,
+            validation_steps = 16,
+            shuffle=True
+            )
+    # evalTrainLoss = history.history['accuracy']
+    # test_loss, test_acc = model.evaluate(test_generator, verbose=2) 
+    # evalTestAcc.append(test_acc)
+    # evalTestLoss.append(test_loss)
+    # numUnits.append(x)
+    # time.append(datetime.now() - start)
+    # print(time,x)
+    # model.save('trainedModel.h5')
 
-print(evalTestAcc,evalTestLoss,numUnits)
-print(evalTrainLoss)
-print(time)
+# print(evalTestAcc,evalTestLoss,numUnits)
+# print(evalTrainLoss)
+# print(time)
 
-x = numUnits
-y1 = evalTestAcc 
+# x = numUnits
+# y1 = evalTestAcc 
 
-figure, axis = plt.subplots(2, 1)
+# figure, axis = plt.subplots(2, 1)
 
-axis[0].plot(x,y1,'o')
-axis[0].set_title("Accuracy")
+# axis[0].plot(x,y1,'o')
+# axis[0].set_title("Accuracy")
 
-y2 = time 
+# y2 = time 
 
-axis[1].plot(x,y2,'o')
-axis[1].set_title("Time")
+# axis[1].plot(x,y2,'o')
+# axis[1].set_title("Time")
 
 
 plt.savefig('figure.png')
